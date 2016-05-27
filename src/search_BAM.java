35a6f739f415fdf76cd72a6c36104773a0c7aba9
ulloav <vulloa@csumb.edu>
2016-05-27 08:49:17 -0700

    comments

--- src/search_BAM.java
+++ src/search_BAM.java
@@ -13,7 +13,7 @@ public class search_BAM {
     public static void main(String[] args) throws IOException  {
 
         searchGUI s = new searchGUI();
-        s.show();
+        //s.show();
 
         //	file objects
         File folder = new File("\\\\stpnascl2n1\\eaiarch\\PRD\\MES_M680_PO.EAI.ARCHIVE\\20160520\\");
